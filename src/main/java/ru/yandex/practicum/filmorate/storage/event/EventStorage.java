e6fe591782259976df6cb21b8855114690c839df
GrettaSmith <49281543+GrettaSmith@users.noreply.github.com>
2023-05-01 18:33:32 +0300

    Update EventStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/event/EventStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/event/EventStorage.java
@@ -5,7 +5,6 @@ import ru.yandex.practicum.filmorate.model.Event;
 import java.util.List;
 
 public interface EventStorage {
-    List<Event> getFeed(int userId);
-    
+    List<Event> getFeed(int userId);    
     void createEvent(Event event);
 }
