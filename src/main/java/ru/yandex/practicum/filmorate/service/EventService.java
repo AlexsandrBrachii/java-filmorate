99ff1b67e3104a8320dd4f6b6141e146e4ca2b93
GrettaSmith <49281543+GrettaSmith@users.noreply.github.com>
2023-05-02 19:37:41 +0300

    Update EventService.java

--- src/main/java/ru/yandex/practicum/filmorate/service/EventService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/EventService.java
@@ -3,22 +3,29 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.constants.EventOperation;
-import ru.yandex.practicum.filmorate.constants.EventType;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Event;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
 import ru.yandex.practicum.filmorate.storage.event.EventStorage;
 
 
 import java.time.Instant;
 import java.util.List;
 
+
 @Service
 @RequiredArgsConstructor
 public class EventService {
-    final EventStorage eventStorage;
+    final private EventStorage eventStorage;
+    final private UserStorage userStorage;
 
     public List<Event> getFeed(int userId) {
-        return eventStorage.getFeed(userId);
+        if (userStorage.getUser(userId)!=null) {
+            return eventStorage.getFeed(userId);
+        }
+        else {
+            throw new NotFoundException("Такого пользователя не существует");
+        }
     }
 
     public void createEvent(int userId, EventType eventType, EventOperation eventOperation, int entityId) {
