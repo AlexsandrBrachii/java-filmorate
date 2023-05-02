4d475571ab29996ba3abcbd57608eec90977c9f0
GrettaSmith <49281543+GrettaSmith@users.noreply.github.com>
2023-05-02 15:41:20 +0300

    Update ReviewService.java

--- src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -22,27 +22,29 @@ public class ReviewService {
     private final FilmService filmService;
     private final EventService eventService;
 
-    public Review addReview(Review review) {
+     public Review addReview(Review review) {
         userService.getUser(review.getUserId());
         filmService.getFilm(review.getFilmId());
         if (review.getIsPositive() == null || review.getContent() == null) {
             throw new ValidationException("Поле isPositive не может быть null");
         }
-        eventService.createEvent(review.getUserId(), EventType.REVIEW, EventOperation.ADD, review.getReviewId());
-        return reviewStorageDb.addReview(review);
+        Review r = reviewStorageDb.addReview(review);
+        eventService.createEvent(r.getUserId(), EventType.REVIEW, EventOperation.ADD, r.getReviewId());
+        return r;
     }
 
-    public Review updateReview(Review review) {
+   public Review updateReview(Review review) {
         userService.getUser(review.getUserId());
         filmService.getFilm(review.getFilmId());
-        eventService.createEvent(review.getUserId(), EventType.REVIEW, EventOperation.UPDATE, review.getReviewId());
-        return reviewStorageDb.updateReview(review);
+        Review r = reviewStorageDb.updateReview(review);
+        eventService.createEvent(r.getUserId(), EventType.REVIEW, EventOperation.UPDATE, r.getReviewId());
+        return r;
     }
 
     public void deleteReview(int reviewId) {
-        reviewStorageDb.deleteReview(reviewId);
         Review review = reviewStorageDb.getReview(reviewId);
         eventService.createEvent(review.getUserId(), EventType.REVIEW, EventOperation.REMOVE, review.getReviewId());
+        reviewStorageDb.deleteReview(reviewId);
     }
 
     public Review getReview(int reviewId) {
