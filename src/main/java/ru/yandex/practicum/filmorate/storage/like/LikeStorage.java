b2a66adbd5f83cab6e6ee4b323583add6228b797
Meg <h.marga.s@mail.ru>
2023-05-02 23:12:57 +0300

    fix: Добавление ленты новостей

--- src/main/java/ru/yandex/practicum/filmorate/storage/like/LikeStorage.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.filmorate.storage.like;
-
-public interface LikeStorage {
-
-    void addLikeToFilm(Integer filmId, Integer userId);
-
-    void deleteLikeFromFilm(Integer filmId, Integer userId);
-}
